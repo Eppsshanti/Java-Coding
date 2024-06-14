8dc6c2fa834dd82bf6d0d1a2b2c25344aae0f023
Shanti E <158221516+Eppsshanti@users.noreply.github.com>
2024-06-14 16:27:30 -0500

    Java boot camp practice
    section 1-3

---
+++ Java-Projects/Section 2/Bus.java
@@ -0,0 +1,14 @@
+public class Bus {
+    public static void main(String[] args) {
+        int foo = 2;
+
+        int bar = foo - 6;
+        foo += bar;
+
+
+        System.out.println(foo);
+        System.out.println(bar);
+    
+    }
+    
+}
---
+++ Java-Projects/Section 2/WholeNumbers.java
@@ -0,0 +1,9 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        long globalPopulation = 8000_000_000L;
+        long dailyGoogleSearches = 8_500_000_000L;
+        System.out.println("The global population is " + globalPopulation);
+        System.out.println("Daily, there are " + dailyGoogleSearches + " google searches");
+
+    }
+}
